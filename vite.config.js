054f772f58cc4aab05a63976e52e7e53210a4135
YakeDev <erickay.dev@gmail.com>
2025-08-21 10:58:15 +0200

    test-12

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ command, mode }) => {
-	return {
-		root: '.', // Racine du projet
-		server: { open: true },
-		base:
-			command === 'build' && process.env.GITHUB_ACTIONS
-				? '/invitations-app/' // <-- adapte avec le nom de ton repo GitHub
-				: '/',
-		build: {
-			outDir: 'dist',
-		},
-	}
+export default defineConfig({
+	base: '/invitations-app/', // <-- ton repo GitHub
+	build: {
+		outDir: 'dist',
+	},
 })
