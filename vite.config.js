ad3fe9a661b15e18e50ac85458174744258a0ac5
YakeDev <erickay.dev@gmail.com>
2025-08-21 10:40:47 +0200

    mise a jour vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import { defineConfig } from 'vite'
 export default defineConfig({
 	root: '.', // Racine du projet
 	server: { open: true },
-	base: '/invitations-app/',
+	base:
+		command === 'build' && process.env.GITHUB_ACTIONS
+			? '/invitations-app/' // <-- adapte avec le nom de ton repo GitHub
+			: '/',
+	build: {
+		outDir: 'dist',
+	},
 })
